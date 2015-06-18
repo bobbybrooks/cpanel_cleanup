b7be46d8830ab49845403b2f1079ad247bc3c840
Bobby <bobby@bobby-brooks.com>
2015-06-18 14:58:12 -0400

    Update diskusage.sh

--- diskusage.sh
+++ diskusage.sh
@@ -5,9 +5,9 @@ domainName=$(echo "ftp.firstdomain.com")
 userName=$(echo "username1")
 # This is 10 MB
 availSpace=$(echo "10000")
-ssh -p 1291 $userName@$domainName -t "du ~ > ./fileusage.txt"
-scp -P 1291 $userName@$domainName:fileusage.txt ~/Documents/Website_Cleanup/$userName-fileusage.txt
-ssh -p 1291 $userName@$domainName -t "rm -rf ~/fileusage.txt"
+ssh -p 1234 $userName@$domainName -t "du ~ > ./fileusage.txt"
+scp -P 1234 $userName@$domainName:fileusage.txt ~/Documents/Website_Cleanup/$userName-fileusage.txt
+ssh -p 1234 $userName@$domainName -t "rm -rf ~/fileusage.txt"
 usedSpace=$(tail -1 $userName-fileusage.txt | sed "s@home@$availSpace@g" | sed 's/\t//g' | cut -d "/" -f 1)
 percentUsed=$(echo "$usedSpace/$availSpace" | bc -l)
 echo "$userName     $percentUsed" >> ./current-diskusage.txt
@@ -20,9 +20,9 @@ domainName=$(echo "ftp.firstdomain.com")
 userName=$(echo "username1")
 # This is 10 MB
 availSpace=$(echo "10000")
-ssh -p 1291 $userName@$domainName -t "du ~ > ./fileusage.txt"
-scp -P 1291 $userName@$domainName:fileusage.txt ~/Documents/Website_Cleanup/$userName-fileusage.txt
-ssh -p 1291 $userName@$domainName -t "rm -rf ~/fileusage.txt"
+ssh -p 1234 $userName@$domainName -t "du ~ > ./fileusage.txt"
+scp -P 1234 $userName@$domainName:fileusage.txt ~/Documents/Website_Cleanup/$userName-fileusage.txt
+ssh -p 1234 $userName@$domainName -t "rm -rf ~/fileusage.txt"
 usedSpace=$(tail -1 $userName-fileusage.txt | sed "s@home@$availSpace@g" | sed 's/\t//g' | cut -d "/" -f 1)
 percentUsed=$(echo "$usedSpace/$availSpace" | bc -l)
 echo "$userName     $percentUsed" >> ./current-diskusage.txt
