22d251229adbac435d90d1aef282578b289acf3a
Bobby <bobby@bobby-brooks.com>
2015-06-17 20:20:01 -0400

    Create site-cleaner.sh

---
+++ site-cleaner.sh
@@ -0,0 +1,3 @@
+# This sequentially connects to firstdomain, seconddomain, and thirddomain and clears the ~/tmp folder.
+# Change port number/username/domain
+ssh -p 1234 username1@ftp.firstdomain.com -t "rm -rf ~/tmp/*"; ssh -p 1234 username2@ftp.seconddomain.com -t "rm -rf ~/tmp/*"; ssh -p 1234 username3@ftp.thirddomain.com -t "rm -rf ~/tmp/*";
