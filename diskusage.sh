e6c5c2d47441312df63787b3673c592beb936cf2
Bobby <bobby@bobby-brooks.com>
2015-06-18 00:20:03 -0400

    Update diskusage.sh

--- diskusage.sh
+++ diskusage.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 # FirstDomain.com
 # Change these variables
-userName=$(echo "username1")
 domainName=$(echo "ftp.firstdomain.com")
-ssh -p 1234 $userName@$domainName -t "du ~ > ./fileusage.txt"
-scp -P 1234 $userName@$domainName:fileusage.txt ~/Documents/Website_Cleanup/$userName-fileusage.txt
-ssh -p 1234 $userName@$domainName -t "rm -rf ~/fileusage.txt"
+userName=$(echo "username1")
+# This is 10 MB
+availSpace=$(echo "10000")
+ssh -p 1291 $userName@$domainName -t "du ~ > ./fileusage.txt"
+scp -P 1291 $userName@$domainName:fileusage.txt ~/Documents/Website_Cleanup/$userName-fileusage.txt
+ssh -p 1291 $userName@$domainName -t "rm -rf ~/fileusage.txt"
+usedSpace=$(tail -1 $userName-fileusage.txt | sed "s@home@$availSpace@g" | sed 's/\t//g' | cut -d "/" -f 1)
+percentUsed=$(echo "$usedSpace/$availSpace" | bc -l)
+echo "$userName     $percentUsed" >> ./current-diskusage.txt
 
 # SecondDomain.com
 # Change these variables
 userName=$(echo "username2")
 domainName=$(echo "ftp.seconddomain.com")
-ssh -p 1234 $userName@$domainName -t "du ~ > ./fileusage.txt"
-scp -P 1234 $userName@$domainName:fileusage.txt ~/Documents/Website_Cleanup/$userName-fileusage.txt
-ssh -p 1234 $userName@$domainName -t "rm -rf ~/fileusage.txt"
+domainName=$(echo "ftp.firstdomain.com")
+userName=$(echo "username1")
+# This is 10 MB
+availSpace=$(echo "10000")
+ssh -p 1291 $userName@$domainName -t "du ~ > ./fileusage.txt"
+scp -P 1291 $userName@$domainName:fileusage.txt ~/Documents/Website_Cleanup/$userName-fileusage.txt
+ssh -p 1291 $userName@$domainName -t "rm -rf ~/fileusage.txt"
+usedSpace=$(tail -1 $userName-fileusage.txt | sed "s@home@$availSpace@g" | sed 's/\t//g' | cut -d "/" -f 1)
+percentUsed=$(echo "$usedSpace/$availSpace" | bc -l)
+echo "$userName     $percentUsed" >> ./current-diskusage.txt
+
+
